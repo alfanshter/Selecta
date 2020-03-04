02a38ca5e928ffd49a366d3e145c69580ded17b6
alfanshter <alfanno8@gmail.com>
2020-03-05 06:24:29 +0700

    update terbaru

--- app/src/main/java/com/alfanshter/jatimpark/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/alfanshter/jatimpark/ui/dashboard/DashboardFragment.kt
@@ -1,33 +1,36 @@
 package com.alfanshter.jatimpark.ui.dashboard
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alfanshter.jatimpark.Model.BannerPromo
-import com.alfanshter.jatimpark.Model.Supplier
+import com.alfanshter.jatimpark.Model.UsersInfo
 import com.alfanshter.jatimpark.R
-import com.alfanshter.jatimpark.adapter.HobbiesAdapter
 import com.alfanshter.jatimpark.item.BannerCarouselItem
 import com.alfanshter.jatimpark.item.BannerListener
-import com.daimajia.slider.library.Animations.DescriptionAnimation
-import com.daimajia.slider.library.SliderLayout
-import com.daimajia.slider.library.SliderTypes.BaseSliderView
-import com.daimajia.slider.library.SliderTypes.TextSliderView
-import com.daimajia.slider.library.Tricks.ViewPagerEx
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.*
+import com.google.firebase.iid.FirebaseInstanceId
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import kotlinx.android.synthetic.main.fragment_dashboard.*
 import org.jetbrains.anko.find
+import org.jetbrains.anko.support.v4.startActivity
 import org.jetbrains.anko.support.v4.toast
 
 
@@ -38,26 +41,30 @@ class DashboardFragment : Fragment(), BannerListener {
     private lateinit var banner: RecyclerView
     private var groupAdapter = GroupAdapter<ViewHolder>()
     lateinit var databaseReference: DatabaseReference
+    lateinit var refinfo : DatabaseReference
+    lateinit var show_progress: ProgressBar
     var bannersatu = ""
-    var promos : List<BannerPromo> = listOf()
+    var promos: List<BannerPromo> = listOf()
     var status = ""
+    @SuppressLint("StringFormatInvalid")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+       
 
         banner = root.find(R.id.rvMain)
         databaseReference = FirebaseDatabase.getInstance().reference.child("Selecta").child("Home")
-        databaseReference.addValueEventListener(object :ValueEventListener, BannerListener {
+        databaseReference.addValueEventListener(object : ValueEventListener, BannerListener {
             override fun onCancelled(p0: DatabaseError) {
 
             }
 
             override fun onDataChange(p0: DataSnapshot) {
                 bannersatu = p0.child("banner/postimage").value.toString()
-                 promos = listOf(
+                promos = listOf(
                     BannerPromo(
                         name = "Puncak badai uang",
                         image = bannersatu
@@ -88,12 +95,11 @@ class DashboardFragment : Fragment(), BannerListener {
                 groupAdapter.add(bannerCarouselItem)
 
 
-
-
             }
 
             override fun onSeeAllPromoClick() {
-                toast("see all promo")            }
+                toast("see all promo")
+            }
 
             override fun onBannerClick(promo: BannerPromo) {
             }
@@ -110,19 +116,70 @@ class DashboardFragment : Fragment(), BannerListener {
 
 
         dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        recyclerView = root.find(R.id.recyclerView)
 
 
+
+        recyclerView = root.find(R.id.recyclerinfo)
+
+        /*
         val LayoutManager = LinearLayoutManager(context!!.applicationContext)
         LayoutManager.orientation = LinearLayoutManager.HORIZONTAL
         recyclerView.layoutManager = LayoutManager
+*/
+        val LayoutManager = LinearLayoutManager(context!!.applicationContext)
+        LayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.layoutManager = LayoutManager
+        refinfo = FirebaseDatabase.getInstance().reference.child("Selecta").child("info")
 
+        val option = FirebaseRecyclerOptions.Builder<UsersInfo>().setQuery(refinfo,UsersInfo::class.java)
+            .build()
+
+        val firebaseRecyclerAdapter = object : FirebaseRecyclerAdapter<UsersInfo,MyViewHolder>(option){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+                val itemView = LayoutInflater.from(context?.applicationContext).inflate(R.layout.list_item,parent,false)
+                return MyViewHolder(itemView)
+            }
+
+            override fun onBindViewHolder(holder: MyViewHolder, position: Int, model: UsersInfo) {
+                val refid = getRef(position).key.toString()
+                refinfo.child(refid).addValueEventListener(object : ValueEventListener{
+                    override fun onCancelled(p0: DatabaseError) {
+
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot) {
+                        holder.mtitle.setText(model.nama)
+                        Picasso.get().load(model.gambar).fit().centerCrop().into(holder.mimage)
+                        holder.itemView.setOnClickListener {
+                            startActivity<information>("Firebase_Image" to model.gambar, "Firebase_title" to model.nama, "Firebase_isi" to model.deskripsi)
+
+
+                        }
+                    }
+
+                })
+            }
+        }
+
+        recyclerView.adapter = firebaseRecyclerAdapter
+        firebaseRecyclerAdapter.startListening()
+
+/*
+        val LayoutManager = LinearLayoutManager(context!!.applicationContext)
+        LayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.layoutManager = LayoutManager
+*/
+
+
+/*
         val adapter = HobbiesAdapter(context!!.applicationContext, Supplier.hobbies)
         recyclerView.adapter = adapter
+*/
 
         return root
 
     }
+
 
     override fun onSeeAllPromoClick() {
         toast("see all promo")
@@ -131,5 +188,9 @@ class DashboardFragment : Fragment(), BannerListener {
     override fun onBannerClick(promo: BannerPromo) {
     }
 
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var mtitle : TextView = itemView.findViewById(R.id.txvTitle)
+        var mimage : ImageView = itemView.findViewById(R.id.gambar)
+    }
 
 }
